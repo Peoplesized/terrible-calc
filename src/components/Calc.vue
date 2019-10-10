b8d910497391fecc7fe4f0fade81ce3d7547346c
Jonathan Smith <jonathan@peoplesized.com>
2019-10-10 14:39:34 +0200

    fine for now

--- src/components/Calc.vue
+++ src/components/Calc.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <p>
-      <label>Value a =</label>
-      <input type="number" @blur='a -= 1' v-model.number="a" />
+      <label>Value a = </label>
+      <input type="number" @blur="change_inputs_on_blur ? a -= 1 : ''" v-model.number="a" />
     </p>
     <p>
-      <label>Value b =</label>
-      <input type="number" @blur='b -= 1' v-model.number="b" />
+      <label>Value b = </label>
+      <input type="number" @blur="change_inputs_on_blur ? b -= 1 : ''" v-model.number="b" />
     </p>
     <p>
       <button @click="add">+</button>
@@ -15,7 +15,13 @@
       <button @click="divide">÷</button>
       <button @click="clear">C</button>
     </p>
-    <p :style='{fontSize: cumulative + "em"}'>Result: {{result}}</p>
+    <p :style="{fontSize: font_size_em + 'em'}">Result: {{result}}</p>
+    <p>
+      <input type="checkbox" v-model="change_inputs_on_blur" />
+      <label
+        style="font-size: 0.5em; color: lightgrey;"
+      >Subtract 1 from each input value after you change it</label>
+    </p>
   </div>
 </template>
 
@@ -28,28 +34,20 @@ export default Vue.extend({
       a: 0,
       b: 0,
       result: 0,
-      calc_count: 0,
-      cumulative: 1,
+      font_size_em: 1,
+      change_inputs_on_blur: true,
+      auto_increment: null as any | null,
     };
   },
   methods: {
     any() {
-      if (this.calc_count >= 2) {
-        alert('Too many calculations - let me rest...');
-        this.randomise_inputs();
-        this.calc_count = 0;
-      }
-      this.calc_count += 1;
-      this.cumulative -= 0.25;
-      if (this.cumulative < 0.5) {
-        this.cumulative = 1;
+      this.font_size_em -= 0.25;
+      if (this.font_size_em < 0.5) {
+        this.font_size_em = 1;
       }
     },
     add() {
       this.any();
-      if (this.a === 0 && this.b === 0) {
-        return this.result = -1;
-      }
       return this.result = this.a + this.b + 10;
     },
     subtract() {
@@ -65,19 +63,30 @@ export default Vue.extend({
     },
     divide() {
       this.any();
-      if (this.a === 0) {
-        return this.b = 999;
-      }
+      // if (this.a === 0) {
+      //   this.a = 1;
+      //   this.b = 999;
+      // }
       this.result = this.a / this.b;
     },
     clear() {
       this.any();
-      if (this.b === 0) {
-        return this.b = Math.random() > 0.6 ? 1 : 0;
+
+      // if (this.b === 0) {
+      //   return this.b = Math.random() > 0.6 ? 1 : 0;
+      // }
+
+      if (this.auto_increment !== null) {
+        clearTimeout(this.auto_increment);
+        this.auto_increment = null;
+        return;
       }
-      setTimeout(() => {
+
+      this.auto_increment = setTimeout(() => {
         this.randomise_inputs();
+        this.auto_increment = null;
       }, 2000);
+
       this.a = 0;
       this.b = 0;
       this.result = 0;
@@ -85,6 +94,10 @@ export default Vue.extend({
     randomise_inputs() {
       this.a = Math.floor(Math.random() * 100);
       this.b = Math.floor(Math.random() * 100);
+    },
+    increment_inputs() {
+      this.a += 1;
+      this.b += 1;
     },
   },
 });
@@ -105,5 +118,8 @@ li {
 }
 a {
   color: #42b983;
+}
+input, button {
+  font-size: 1em;
 }
 </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="about">
     <h1>About TerribleCalc</h1>
+    <p>World's first SaaS calculator. Try it out for free and hit <router-link to='signup'>signup</router-link> to start a subscription</p>
     <p>This calculator was made quickly and without care or attention. But you can definitely rely on it for all your calculation needs, including moon landings, mergers and acquisitions, expensive science experiments, etc.</p>
     <a href="https://www.google.com">Start calculating</a>
   </div>
